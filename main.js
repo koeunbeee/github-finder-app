251061285504a21f43fbfbe71a7be08a22053746
Bella <kbee1202@gmail.com>
2023-11-28 18:26:07 +0900

    html,css 수정

---
+++ main.js
@@ -0,0 +1,33 @@
+const searchInput = document.querySelector('.search-Box');
+const profileImg = document.querySelector('.profile-img');
+const viewProfileButton = document.querySelector('.view-profile-button');
+const reposCount = document.querySelector('#public-repos');
+const gistsCount = document.querySelector('#public-gists');
+const followersCount = document.querySelector('#followers');
+const followingCount = document.querySelector('#following');
+const companyName = document.querySelector('#company');
+const websiteAdrres = document.querySelector('#website');
+const location = document.querySelector('#location');
+const memberSince = document.querySelector('#member-since');
+const reposList = document.querySelector('.repos-list');
+const repoName = document.querySelector('#repo-name');
+const starsCount = document.querySelector('#stars');
+const watchersCount = document.querySelector('#watchers');
+const forksCount = document.querySelector('#forks');
+
+// function User(
+//   profileName,
+//   repo,
+//   gist,
+//   follower,
+//   following,
+//   company,
+//   website,
+//   location,
+//   memberSince,
+//   stars,
+//   watchers,
+//   forks
+// ) {
+//   this.profileName;
+// }
