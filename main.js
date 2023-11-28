98fc8eb8afd4216c86830745b5eb5527475a48bf
Bella <kbee1202@gmail.com>
2023-11-29 01:14:42 +0900

    js 수정..

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const followersCount = document.querySelector('#followers');
 const followingCount = document.querySelector('#following');
 const companyName = document.querySelector('#company');
 const websiteAdrres = document.querySelector('#website');
-const location = document.querySelector('#location');
+const locationAdrres = document.querySelector('#location');
 const memberSince = document.querySelector('#member-since');
 const reposList = document.querySelector('.repos-list');
 const repoName = document.querySelector('#repo-name');
@@ -74,4 +74,23 @@ class User {
 
 class Repos {
   repoName;
+  starsCount;
+  watchersCount;
+  forksCount;
+  constructor(name, stargazersCount, watchersCount, forksCount) {
+    this.repoName = name;
+    this.starsCount = stargazersCount;
+    this.watchersCount = watchersCount;
+    this.forksCount = forksCount;
+  }
 }
+
+//  searchInput.addEventListener('keydown', (e) => {
+//   console.log('아');
+//   const searchName = e.target.value;
+//   try {
+//     const search = await axios.get('https://api.github.com/users/' + searchName);
+//   } catch (error) {
+//     console.log('not found');
+//   }
+//  });
